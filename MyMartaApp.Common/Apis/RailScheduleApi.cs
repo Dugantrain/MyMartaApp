bd39d72d65d8127601f4e7fe87f4b0f4796d8125
Dugan <Michael.Dugan@ATL01MDUGAN.internaldomain.com>
2015-10-04 22:24:40 -0400

    RailSchedule value object

--- MyMartaApp.Common/Apis/RailScheduleApi.cs
+++ MyMartaApp.Common/Apis/RailScheduleApi.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using MyMartaApp.Common.ValueObjects;
 
 namespace MyMartaApp.Common.Apis
 {
--- MyMartaApp.Common/ValueObjects/RailSchedule.cs
+++ MyMartaApp.Common/ValueObjects/RailSchedule.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MyMartaApp.Common.ValueObjects
 {
     public class RailSchedule : IValueObject
     {
+        [JsonProperty("DESTINATION")]
+        public string Destination { get; set; }
+        [JsonProperty("DIRECTION")]
+        public string Direction { get; set; }
+        [JsonProperty("EVENT_TIME")]
+        public string EventTime { get; set; }
+        [JsonProperty("LINE")]
+        public string Line { get; set; }
+        [JsonProperty("NEXT_ARR")]
+        public DateTime NextArrival { get; set; }
+        [JsonProperty("STATION")]
+        public string Station { get; set; }
+        [JsonProperty("TRAIN_ID")]
+        public int TrainId { get; set; }
+        [JsonProperty("WAITING_SECONDS")]
+        public int WaitingSeconds { get; set; }
+        [JsonProperty("WAITING_TIME")]
+        public string WaitingTime { get; set; }
     }
 }
